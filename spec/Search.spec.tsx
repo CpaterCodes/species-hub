14adadea08c394ca51831e12a327bb1cc2804305
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-12-20 10:24:50 +0000

    Refactor: Vitest tests now operate asynchronously

--- spec/Search.spec.tsx
+++ spec/Search.spec.tsx
@@ -6,19 +6,23 @@ import { expect, beforeEach, describe, test} from 'vitest';
 describe('Search', function(){
 	test(
 		"It will initialise with no values",
-		function(){
-			render(<Search getSuggestions={() => ['camel']} />);
-			expect(screen.queryByText(/camel/i)).not.toBeInTheDocument();
+		async function(){
+			await render(<Search getSuggestions={() => ['camel']} />);
+			expect(
+				await screen.queryByText(/camel/i)
+			).not.toBeInTheDocument();
 		}
 	);
 
 	test(
 		"It will present suggestions with changing input",
-		function(){
-			render(<Search getSuggestions={() => ['camel']} />);
+		async function(){
+			await render(<Search getSuggestions={() => ['camel']} />);
 			const searchBar = screen.getByRole("search-bar");
 			fireEvent.change(searchBar, {target: {value: "ca"}});
-			expect(screen.queryByText(/camel/i)).toBeInTheDocument();
+			expect(
+				await screen.queryByText(/camel/i)
+			).toBeInTheDocument();
 		}
 	);
 });
