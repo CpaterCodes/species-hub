0981e78753b4df3d85d84f171502edb7b375a135
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-12-22 22:56:19 +0000

    Feat: Tests amended to be less brittle. Info requirements hitherto for Profile component fulfilled, but tests may become more specific with regards to desired layout in future. Could also focus on styling and perhaps even a detailed mockup of the desired layout, but up-front design is currently being avoided.

--- spec/Profile.spec.tsx
+++ spec/Profile.spec.tsx
@@ -1,10 +1,10 @@
 import "@testing-library/jest-dom/vitest";
 import Profile from "../src/components/Profile";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { expect, describe, test, beforeAll} from 'vitest';
+import { expect, describe, test, beforeEach} from 'vitest';
 
 describe("Profile", function(){
-	beforeAll(
+	beforeEach(
 		async function(){ 
 			const taxonomy = [
 				"Kingdom", "Phylum", "Order", "Family", "Genus", "Species"
@@ -24,20 +24,22 @@ describe("Profile", function(){
 		for (const taxon of [
 			"Kingdom", "Phylum", "Order", "Family", "Genus", "Species"
 		]){
-			const taxonDisplay = await screen.getByText(taxon);
+			const taxonDisplay = await screen.getByText(
+				new RegExp(taxon, "i")
+			);
 			expect(taxonDisplay).toBeInTheDocument();
 		}
 	});
 
 	test("It will present endangerment status", async function(){
 		const endangerment = await screen.getByText(
-			"Status: least concern"
+			/Status: least concern/i
 		);
 		expect(endangerment).toBeInTheDocument();
 	});
 
 	test("It will show the common name", async function(){
-		const commonName = await screen.getByText("Animal");
+		const commonName = await screen.getByText(/Animal/i);
 		expect(commonName).toBeInTheDocument();
 	});
 });
