c72a44c49d49b5a9be3ac899a66d181cae8b5766
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-12-22 18:58:03 +0000

    Refactor: Use of beforeEachto manage renders of component for test

--- spec/Search.spec.tsx
+++ spec/Search.spec.tsx
@@ -4,10 +4,13 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { expect, beforeEach, describe, test} from 'vitest';
 
 describe('Search', function(){
+	beforeEach(async function(){
+		await render(<Search getSuggestions={() => ['camel']} />);
+	});
+
 	test(
 		"It will initialise with no values",
 		async function(){
-			await render(<Search getSuggestions={() => ['camel']} />);
 			expect(
 				await screen.queryByText(/camel/i)
 			).not.toBeInTheDocument();
@@ -17,7 +20,6 @@ describe('Search', function(){
 	test(
 		"It will present suggestions with changing input",
 		async function(){
-			await render(<Search getSuggestions={() => ['camel']} />);
 			const searchBar = screen.getByRole("search-bar");
 			fireEvent.change(searchBar, {target: {value: "ca"}});
 			expect(
